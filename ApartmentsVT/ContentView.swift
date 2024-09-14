a1f25ea4f2a001a7c1236064b073768651d5183e
steadman1 <steadman@vt.edu>
2024-09-14 09:32:25 -0400

    deleted SwiftData boilerplate

--- ApartmentsVT/ContentView.swift
+++ ApartmentsVT/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
 
     var body: some View {
         VStack {
